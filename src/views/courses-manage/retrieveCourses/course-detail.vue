366e4069251b2c05f9eabf7f07d5376f26ddd3cc
happy <yuqing.gao@happyelements.com>
2021-04-13 13:20:39 +0800

    章节列表添加操作

--- src/views/courses-manage/retrieveCourses/course-detail.vue
+++ src/views/courses-manage/retrieveCourses/course-detail.vue
@@ -11,11 +11,44 @@
                     <Button @click="createNew"
                             style="float:right;margin-bottom:10px;"
                             icon="ios-plus-outline"
-                            type="primary">新增分类</Button>
+                            type="primary">添加章节</Button>
                 </div>
                 <Table stripe
                     :columns="columns"
                     :data="tableData"></Table>
+                <Modal v-model="showModal"
+               title='编辑章节'
+               @on-visible-change='handleBeforeClose'
+               width="360">
+            <Form ref="newData"
+                  :model="newData"
+                  :rules="ruleValidate"
+                  :label-width="80">
+                <FormItem label="章节名称"
+                          prop="name">
+                    <Input v-model="newData.name"
+                           placeholder="请输入章节名称"></Input>
+                </FormItem>
+                <!-- <FormItem label="链接URL">
+                    <Input v-model="newData.link"
+                           placeholder="请输入链接URL"></Input>
+                </FormItem> -->
+                <!-- <FormItem label="状态">
+                    <Select v-model="newData.status"
+                            style="width:200px">
+                        <Option :value="1">公开</Option>
+                        <Option :value="0">隐藏</Option>
+                    </Select>
+                </FormItem> -->
+            </Form>
+            <div slot="footer">
+                <Button type="primary"
+                        :loading="loading"
+                        @click="handleSubmit">确认</Button>
+                <Button type="dashed"
+                        @click="handleCancel">取消</Button>
+            </div>
+        </Modal>
             </div>
         </Split>
     </div>
@@ -29,9 +62,9 @@
             return {
                 split1: 0.3,
                 tableData: [],
-                // showModal: false,
+                showModal: false,
                 newData: {
-                    status: 1
+                    // status: 1
                 },
                 loading: false,
                 ruleValidate: {
@@ -47,7 +80,49 @@
                     title: '题目',
                     align: 'center',
                     render: (h, params) => {
-                        return h('span', params.row.articles.length || '-');
+                        return h('span', '题目');
+                    }
+                },
+                {
+                    title: '操作',
+                    align: 'center',
+                    key: 'address',
+                    render: (h, params) => {
+                        return h('div', [
+                            h(
+                                'Button',
+                                {
+                                    props: {
+                                        type: 'primary',
+                                        size: 'small'
+                                    },
+                                    style: {
+                                        marginRight: '5px'
+                                    },
+                                    on: {
+                                        click: () => {
+                                            this.editorRow(params.row);
+                                        }
+                                    }
+                                },
+                                '编辑'
+                            ),
+                            h(
+                                'Button',
+                                {
+                                    props: {
+                                        type: 'error',
+                                        size: 'small'
+                                    },
+                                    on: {
+                                        click: () => {
+                                            this.handelCategoryStatus(params.row.chapterId, 0);
+                                        }
+                                    }
+                                },
+                                '删除'
+                            )
+                        ]);
                     }
                 }
             ]
@@ -93,22 +168,23 @@
             });
         },
         createNew() {
-            // this.showModal = true;
+            this.showModal = true;
             this.isCreate = true;
             this.newData = {};
         },
         editorRow(row) {
-            // this.showModal = true;
+            this.showModal = true;
             this.newData = { ...row };
             this.isCreate = false;
         },
         handleSubmit() {
             this.$refs.newData.validate(valid => {
+                console.log(valid)
                 if (valid) {
                     this.loading = true;
-                    this.newData.link = this.newData.link || this.newData.name;
-                    let {_id, name, link ,status} = this.newData
-                    let params = {_id, name, link ,status}
+                    // this.newData.link = this.newData.link || this.newData.name;
+                    let {_id, name} = this.newData
+                    let params = {_id, name}
                     this.isCreate &&
                         addCategory(params).then(
                             res => {
@@ -139,17 +215,17 @@
         },
         handleCancel() {
             this.$refs['newData'].resetFields();
-            this.newData = { status: 1 };
+            // this.newData = { status: 1 };
             this.loading = false;
-            // this.showModal = false;
+            this.showModal = false;
         },
         handleBeforeClose(bool) {
             if (bool) {
                 this.$refs.newData.resetFields();
             }
         },
-        handelCategoryStatus(id, status) {
-            editCategory({ _id: id, status: status }).then(
+        handelCategoryStatus(id) {
+            editCategory({ _id: id}).then(
                 res => {
                     this.$Message.success(res.data.desc);
                     this.getList();
